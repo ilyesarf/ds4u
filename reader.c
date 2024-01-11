8352e158625bddcfe16b22f747d0b71fc18b8861
Quimzy <bam.boy2020@gmail.com>
2024-01-11 12:48:23 +0300

    finished USB device setup

--- reader.c
+++ reader.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <libusb-1.0/libusb.h>
 
 #define VENDORID  0x054c
 #define PRODUCTID 0x09cc
-#define INTRF_N 3 //DS4 has 4 interfaces (0-2 audio, 3 HID)
+#define HID_INTRF 3
 
 void check_err(libusb_context *context, libusb_device_handle *handle, int err, char* err_msg){
     if (err != LIBUSB_SUCCESS) {
         fprintf(stderr, "%s error: %d\n", err_msg, err);
         libusb_close(handle);
         libusb_exit(context);
-        return 1;
+        exit(1);
     }
 }
+
+
 void connect_device(libusb_context *context, libusb_device_handle **handle){
     int init_err = libusb_init(&context);
-    check_err(context, handle, init_err, "Failed to initiate libusb.");
+    check_err(context, *handle, init_err, "Failed to initiate libusb.");
 
     *handle = libusb_open_device_with_vid_pid(context, VENDORID, PRODUCTID);
     if (*handle == NULL) {
         fprintf(stderr, "Failed to open USB device. Try running program as superuser.\n");
         libusb_exit(context);
+        exit(1);
     } 
 }
 
@@ -29,7 +33,7 @@ void print_device_info(libusb_context *context, libusb_device_handle *handle){
     struct libusb_device_descriptor device_desc;
     int device_desc_err = libusb_get_device_descriptor(libusb_get_device(handle), &device_desc);
 
-    check_err(context, handle, device_desc_err, "Failed to get device device_desc.");
+    check_err(context, handle, device_desc_err, "Failed to get device descriptor.");
 
     printf("Vendor ID: 0x%04x\n", device_desc.idVendor);
     printf("Product ID: 0x%04x\n", device_desc.idProduct);
@@ -39,7 +43,7 @@ void print_device_info(libusb_context *context, libusb_device_handle *handle){
 void detach_device(libusb_context *context, libusb_device_handle *handle, int intrf_n){
     int detach_err;
 
-    for (int i =0; i <= intrf_n; i++){
+    for (int i =0; i < intrf_n; i++){
         if (libusb_kernel_driver_active(handle, i) == 1){
             detach_err = libusb_detach_kernel_driver(handle, i);
             
@@ -50,29 +54,46 @@ void detach_device(libusb_context *context, libusb_device_handle *handle, int in
 
 }
 
-void connect_intrf(libusb_context *context, libusb_device_handle *handle, struct libusb_config_descriptor *conf_desc, int intrf){
-    int claim_err = libusb_claim_interface(handle, intrf); 
+void get_config_info(libusb_context *context, libusb_device_handle *handle, struct libusb_config_descriptor **conf_desc){
+    int conf_err = libusb_get_config_descriptor(libusb_get_device(handle), 0, conf_desc);
+    check_err(context, handle, conf_err, "Failted to get configuration descriptor.");
+    
+}
+
+void claim_intrf(libusb_context *context, libusb_device_handle *handle, int intrf_n){
+    int claim_err = libusb_claim_interface(handle, intrf_n); 
 
     check_err(context, handle, claim_err, "Failed to claim interface");
 
-    printf("interface number %d claimed\n", intrf);
-
-    struct libusb_config_descriptor *conf_desc;
-    //struct libusb_interface_descriptor intrf_desc;
-    int conf_err = libusb_get_config_descriptor(libusb_get_device(handle), 0, &conf_desc);
-    
 }
+
 
 int main() {
     libusb_context *context = NULL;
     libusb_device_handle *handle = NULL;
 
     connect_device(context, &handle);
+    printf("device conntected successfully\n");
     
     print_device_info(context, handle);
 
-    detach_device(context, handle, INTRF_N);
-    
+    struct libusb_config_descriptor *conf_desc;
+
+    get_config_info(context, handle, &conf_desc);
+    int intrf_n = conf_desc->bNumInterfaces;
+    printf("device has %d interfaces\n", intrf_n);
+
+    detach_device(context, handle, intrf_n);
+    printf("all interfaces are detached from kernel drivers\n");
+
+    claim_intrf(context, handle, HID_INTRF);
+    printf("interface number %d is claimed\n", intrf_n);
+
+    struct libusb_endpoint_descriptor in_ep = conf_desc->interface[HID_INTRF].altsetting->endpoint[0];
+    struct libusb_endpoint_descriptor out_ep = conf_desc->interface[HID_INTRF].altsetting->endpoint[1];
+
+    printf("in endpoint address: %d\n", in_ep.bEndpointAddress);
+    printf("out endpoint address: %d\n", out_ep.bEndpointAddress);
 
     libusb_close(handle);
     libusb_exit(context);
