3194d7d705ab8b0ec39b4f749e8dc7eefe0ee93c
Quimzy <bam.boy2020@gmail.com>
2024-01-11 11:56:34 +0300

    better error handling

--- reader.c
+++ reader.c
@@ -3,13 +3,19 @@
 
 #define VENDORID  0x054c
 #define PRODUCTID 0x09cc
-#define INTRF_N 3 //DS4 has 3 interfaces
+#define INTRF_N 3 //DS4 has 4 interfaces (0-2 audio, 3 HID)
 
+void check_err(libusb_context *context, libusb_device_handle *handle, int err, char* err_msg){
+    if (err != LIBUSB_SUCCESS) {
+        fprintf(stderr, "%s error: %d\n", err_msg, err);
+        libusb_close(handle);
+        libusb_exit(context);
+        return 1;
+    }
+}
 void connect_device(libusb_context *context, libusb_device_handle **handle){
     int init_err = libusb_init(&context);
-    if (init_err < LIBUSB_SUCCESS) {
-        fprintf(stderr, "Failed to initialize libusb\n");
-    }
+    check_err(context, handle, init_err, "Failed to initiate libusb.");
 
     *handle = libusb_open_device_with_vid_pid(context, VENDORID, PRODUCTID);
     if (*handle == NULL) {
@@ -20,17 +26,14 @@ void connect_device(libusb_context *context, libusb_device_handle **handle){
 
 
 void print_device_info(libusb_context *context, libusb_device_handle *handle){
-    struct libusb_device_descriptor desc;
-    int desc_err = libusb_get_device_descriptor(libusb_get_device(handle), &desc);
-    if ( desc_err < LIBUSB_SUCCESS) {
-        fprintf(stderr, "Failed to get device desc. error: %d\n", desc_err);
-        libusb_close(handle);
-        libusb_exit(context);
-    }
+    struct libusb_device_descriptor device_desc;
+    int device_desc_err = libusb_get_device_descriptor(libusb_get_device(handle), &device_desc);
 
-    printf("Vendor ID: 0x%04x\n", desc.idVendor);
-    printf("Product ID: 0x%04x\n", desc.idProduct);
-    printf("Number of Products: %d\n", desc.bNumConfigurations);
+    check_err(context, handle, device_desc_err, "Failed to get device device_desc.");
+
+    printf("Vendor ID: 0x%04x\n", device_desc.idVendor);
+    printf("Product ID: 0x%04x\n", device_desc.idProduct);
+    printf("Number of Configurations: %d\n", device_desc.bNumConfigurations);
 }
 
 void detach_device(libusb_context *context, libusb_device_handle *handle, int intrf_n){
@@ -39,17 +42,26 @@ void detach_device(libusb_context *context, libusb_device_handle *handle, int in
     for (int i =0; i <= intrf_n; i++){
         if (libusb_kernel_driver_active(handle, i) == 1){
             detach_err = libusb_detach_kernel_driver(handle, i);
-            if (detach_err < LIBUSB_SUCCESS){
-                fprintf(stderr, "Failed to detach device from kernel driver. error: %d\n", detach_err);
-                libusb_close(handle);
-                libusb_exit(context);
-            }
+            
+            check_err(context, handle, detach_err, "Failed to detach device from kernel driver.");
             printf("%d interf is detached\n", i);
         }
     }
 
 }
 
+void connect_intrf(libusb_context *context, libusb_device_handle *handle, struct libusb_config_descriptor *conf_desc, int intrf){
+    int claim_err = libusb_claim_interface(handle, intrf); 
+
+    check_err(context, handle, claim_err, "Failed to claim interface");
+
+    printf("interface number %d claimed\n", intrf);
+
+    struct libusb_config_descriptor *conf_desc;
+    //struct libusb_interface_descriptor intrf_desc;
+    int conf_err = libusb_get_config_descriptor(libusb_get_device(handle), 0, &conf_desc);
+    
+}
 
 int main() {
     libusb_context *context = NULL;
@@ -60,6 +72,7 @@ int main() {
     print_device_info(context, handle);
 
     detach_device(context, handle, INTRF_N);
+    
 
     libusb_close(handle);
     libusb_exit(context);
